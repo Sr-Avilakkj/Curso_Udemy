daa8a7a3084cc90284b5d986d86971051599c78b
Isaac Ávila <50557305+Sr-Avilakkj@users.noreply.github.com>
2024-09-11 16:25:59 -0300

    Add files via upload

---
+++ Project_01/main.js
@@ -0,0 +1,1 @@
+console.log('Tenho 20 anos')
---
+++ Project_02/main.js
@@ -0,0 +1,7 @@
+let firstName = 'Isaac'
+
+let lastName = 'Avila'
+
+let age = '20'
+
+console.log('Me chamo' + firstName + lastName + 'e tenho' + age  + 'anos')
