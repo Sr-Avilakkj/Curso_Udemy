8d69c1a987537e8fe28ac3e0d7135ef8285a3695
Isaac Ávila <50557305+Sr-Avilakkj@users.noreply.github.com>
2024-09-11 16:35:57 -0300

    Update main.js

--- Project_02/main.js
+++ Project_02/main.js
@@ -4,4 +4,4 @@ let lastName = 'Avila'
 
 let age = '20'
 
-console.log('Me chamo' + firstName + lastName + 'e tenho' + age  + 'anos')
+console.log('Me chamo ' + firstName + ' ' + lastName + ' e tenho ' + age  + ' anos')
