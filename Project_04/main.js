bd2c6af415104a01f36ce22942640407333950bf
Isaac Ávila <50557305+Sr-Avilakkj@users.noreply.github.com>
2024-09-12 13:05:33 -0300

    Sr.Avilakkj

---
+++ Project_04/main.js
@@ -0,0 +1,35 @@
+let completeName = prompt('Digite seu nome completo:')
+let age = Number(prompt('Digite sua idade:')) + 3
+
+console.log('Ola, ' + completeName + '\nVoce tem ' + age + ' anos de idade')
+
+
+
+// Essa foi a progressao dos meus testes ate chegar no codigo final (Sem pesquisas, apenas tentando na marra mesmo):
+
+// |let completeName = prompt('Digite seu nome completo:')
+// |let age = Number(prompt('Digite sua idade:'))
+// |let ageS = age + 3
+// |console.log('Ola, ' + completeName + '\nVoce tem ' + ageS + ' anos de idade')
+
+
+
+// |let completeName = prompt('Digite seu nome completo:')
+// |let age = Number(prompt('Digite sua idade:'))
+// |age += 3
+// |console.log('Ola, ' + completeName + '\nVoce tem ' + age + ' anos de idade')
+
+
+
+// Esse e o do professor:
+
+// |let firstName = prompt('Digite o seu primeiro nome:')
+// |let lastName = prompt('Digite o seu sobrenome nome:')
+// |let age = prompt('Digite a sua idade:')
+
+// |age = age + 2
+
+
+// |console.log(firstName)
+// |console.log(lastName)
+// |console.log(age)
