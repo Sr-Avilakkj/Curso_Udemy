c0ade1bfdbc778b1e2315e9b94a72fc269f2500a
Isaac Ávila <50557305+Sr-Avilakkj@users.noreply.github.com>
2024-09-12 00:02:05 -0300

    #

---
+++ Languages/JS/main.js
@@ -0,0 +1,15 @@
+console.log('Retorno'))
+
+prompt('Entrada:')
+
+let variableOne = 'Variavel' 
+
+let variableTwo = prompt('Entrada vinculada a uma variavel:')
+
+let variableThree = Number(prompt('Entrada de numero:'))
+
+const VariableFour = 'Variavel posinvariavel'
+
+var VariableFive = 'Variavel'
+
+// comentarios
