5c740f538ebcc99c1b5416c20c974437a9810c17
Isaac Ávila <50557305+Sr-Avilakkj@users.noreply.github.com>
2024-09-14 01:12:29 -0300

    Update by Sr.Avilakkj

---
+++ Project_06/main (part. 1 - test).js
@@ -0,0 +1,22 @@
+// Criei uma lista
+let array_01 = ['Isaac', 'Bruna', 'Lupita', 'Clark', 'Maria Eduarda', 1, true, false]
+
+// Selecionei o elemento da lista que iria ser aprensentado apos a apresentacao da propria lista
+console.log(array_01)
+console.log(array_01[0])
+
+
+// Alterei os elementos da lista durante a execucao do programa
+array_01 = ['Caasi', 'anurB', 'atipuL', 'kralC', 'adraudE airaM']
+
+// O mesmo do exemplo anterior
+console.log(array_01)
+console.log(array_01[0])
+
+
+// Modifiquei appenas um elemento dessa vez
+array_01[0] = 'Isaac'
+
+// Nem preciso explicar, ne?
+console.log(array_01)
+console.log(array_01[0])
---
+++ Project_06/main.js
@@ -0,0 +1,13 @@
+// Entrada do elemento e sua posicao
+let element_01 = prompt('Digite um elemento para por na lista:')
+let position_element_01 = prompt('Digite uma posicao para por o seu elemento na lista (de 0 a 5):')
+
+// Definindo a lista
+let array_01 = [' ', ' ', ' ', ' ', ' ', ' ']
+
+// Executando a isercao do elemento indicado na posicao indicada
+array_01[position_element_01] = element_01
+
+// Vai pro krl, caso queira explicacao dnv disso kkkkkk (brincadeiras a parte, to com sono e to tentando nao cair no tedio (01:06 AM))
+console.log(array_01)
+console.log(array_01[position_element_01])
