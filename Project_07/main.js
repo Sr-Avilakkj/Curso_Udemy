4e27fbb3fb9841a9900b50ca22a32ab513abddad
Isaac Ávila <50557305+Sr-Avilakkj@users.noreply.github.com>
2024-09-19 22:15:25 -0300

    Update by Sr.Avilakkj

---
+++ Project_07/main.js
@@ -0,0 +1,13 @@
+// Programa que converte Celsius para Fahrenheit, porem informando o resultado em 3 locais diferentes (Alert, Console e na Pagina)
+
+let celsius = parseFloat(prompt('Digite a temperatura em graus celsius (Use "." ao inves de "," para marcar a casa decimal'))
+
+console.log('A temperatura informada em celsius, foi de: ' + celsius + '°')
+
+let fahrenheit = (celsius * 1.8) + 32
+
+console.log('Em Fahrenheit: ' + fahrenheit)
+
+document.getElementById('celsius').innerHTML = ('A temperatura informada em celsius, foi de: ' + celsius + '°. ' + 'Em Fahrenheit: ' + fahrenheit + '°')
+
+alert(celsius + '° em Fahrenheit: ' + fahrenheit + '°')
